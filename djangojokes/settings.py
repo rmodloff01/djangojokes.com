bc6d24b52568191753ea7294364bead7b93f0345
Reese Modloff <reesemodloff27@yahoo.com>
2024-08-23 15:49:09 -0500

    Lesson 5 Step 2

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'djangojokes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jokes',
+        'USER': 'postgres',
+        'PASSWORD': 'Aurora27',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 

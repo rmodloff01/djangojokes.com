4b114068a48e749c0788d09b5ccb871453ef0bbb
Reese Modloff <reesemodloff27@yahoo.com>
2024-08-24 14:34:30 -0500

    Lesson 8 Step 13

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -40,12 +40,19 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'crispy_forms',
+    'crispy_bootstrap5',
+
     'common.apps.CommonConfig',
     'jobs.apps.JobsConfig',
     'jokes.apps.JokesConfig',
     'pages.apps.PagesConfig',
     'users.apps.UsersConfig',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

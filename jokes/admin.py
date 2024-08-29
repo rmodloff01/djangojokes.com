8225a6e860345dfc4e0d6dc7d2d7e8154a12af56
Reese Modloff <reesemodloff27@yahoo.com>
2024-08-29 16:21:54 -0500

    Lesson 17 Step 11

--- jokes/admin.py
+++ jokes/admin.py
@@ -5,7 +5,17 @@ from .models import Category, Joke, Tag
 @admin.register(Joke)
 class JokeAdmin(DjangoJokesAdmin):
     model = Joke
-    list_display = ['question', 'created', 'updated']
+
+    # List Attributes
+    date_hierarchy = 'updated'
+    list_display = ['question', 'category', 'updated']
+    list_filter = ['updated', 'category', 'tags']
+    ordering = ['-updated']
+    search_fields = ['question', 'answer']
+
+    # Form Attributes
+    autocomplete_fields = ['tags', 'user']
+    radio_fields = { 'category': admin.HORIZONTAL}
 
     def get_readonly_fields(self, request, obj=None):
         if obj: # editing an existing object
@@ -28,6 +38,8 @@ class TagAdmin(admin.ModelAdmin):
     model = Tag
     list_display = ['tag', 'created', 'updated']
 
+    search_fields = ['tag']
+    
     def get_readonly_fields(self, request, obj=None):
         if obj: # editing an existing object
             return ('slug', 'created', 'updated')
